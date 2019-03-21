6527a2eb033b1f5544eed30682f23481130e2533
andreivercosa <andreivercosa@gmail.com>
2019-03-20 21:15:04 -0400

    v1

---
+++ CepApp/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+using CepApp.Model;
+
+
+namespace CepApp
+{
+    public partial class MainPage : ContentPage
+    {
+        async void Handle_Completed(object sender, System.EventArgs e)
+        {
+            var client = new HttpClient();
+            string cep = txtCEP.Text;
+            var json = await client.GetStringAsync($"https://viacep.com.br/ws/{cep}/json/");
+            var dados = JsonConvert.DeserializeObject<Endereco>(json);
+
+            lblLogradouro.Text = dados.logradouro;
+            lblComplemento.Text = dados.complemento;
+            lblBairro.Text = dados.bairro;
+            lblLocalidade.Text = dados.localidade;
+            lblUF.Text = dados.uf;
+            lblUnidade.Text = dados.unidade;
+            lblIBGE.Text = dados.ibge;
+            lblGIA.Text = dados.gia;
+
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
